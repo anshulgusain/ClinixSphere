050a1a2d7ba05a56c545ef547694bba766c08982
anshulgusain <anshulgusain99@gmail.com>
2024-09-18 15:47:43 +0530

    Made navbar for frontend

---
+++ Frontend/my_app/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../css/navbar.css"
+import Hamburger from "./logo.jsx";
+import { FaSearch } from "react-icons/fa";
+
+
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+      ClinicSphere
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">Create</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li> */}
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+
+            <li>
+              <input type="text" placeholder="Search by id" className="search" />
+              <button className="search-button"><FaSearch /></button>
+            </li>
+            {/* <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+
+export default Navbar
